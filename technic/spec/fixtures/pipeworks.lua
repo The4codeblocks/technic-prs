d6f6977f9e01fedd74f456d8dcc1fcc43118db8c
The4spaceconsts <the3spaceconstants@gmail.com>
2026-07-24 06:48:01 -0500

    fix tests

--- technic/spec/fixtures/pipeworks.lua
+++ technic/spec/fixtures/pipeworks.lua
@@ -2,6 +2,7 @@ mineunit:set_modpath("pipeworks", "spec/fixtures")
 
 _G.pipeworks = {}
 _G.pipeworks.button_label = ""
+_G.pipeworks.button_base = ""
 _G.pipeworks.fs_helpers = {}
 _G.pipeworks.fs_helpers.cycling_button = function(...) return "" end
 
